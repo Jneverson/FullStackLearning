7e06d791b300526de937962acbf215ff3b4eeaaa
Jneverson <jdneverson97@gmail.com>
2018-06-11 07:20:34 -0400

    200 % Training Efficiency Increase
    Previously it took neearly 10000 epochs to reach approxiately 40% - 50 % accuracy, now with only 100 epochs, 43% Accuracy has been reached. With 1000 epochs we have 63.5% Accuracy

--- user_trials_kera.py
+++ user_trials_kera.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 from sklearn.preprocessing import LabelEncoder
+import keras.optimizers as optimizer
 # from sklearn import metrics
 # import os
 featurelen = 17
@@ -53,7 +54,7 @@ dummy_y = np_utils.to_categorical(encoded_Y)  ####IMPORTANT FOR REPORT
 
 '''
 
-x_train, x_test, y_train, y_test = train_test_split(logits, labels_onehot, test_size = 0.25, random_state = 42)
+x_train, x_test, y_train, y_test = train_test_split(logits, labels_onehot, test_size = 0.30, random_state = 42, shuffle = True)
 
 print(type(x_train))
 dir(x_train)
@@ -64,14 +65,17 @@ model.add(Dense(units = 100, activation = 'relu'))
 model.add(Dense(units = 100, activation = 'relu'))
 model.add(Dense(units = len(labels_onehot[-1]), activation = 'softmax'))
 
-model.compile(loss = 'categorical_crossentropy', optimizer = "adam")
+model.compile(loss = 'categorical_crossentropy', optimizer = optimizer.Adam())
 
-
-model.fit(x = x_train, y = y_train, validation_data = (x_test, y_test), verbose = 0, epochs = 1000, batch_size = 5147)
+# batch_size = 5147
+model.fit(x = x_train, y = y_train, validation_data = (x_test, y_test), verbose = 0, epochs = 1000, batch_size = 1000)
 #Correcting the batch_size improved the accuracy to approx. 6 % for 10 epochs)
 
 pred = model.predict(x_test)
+print("Pred 1 : " + str(pred))
 pred = np.argmax(pred, axis = 1)
+print("Pred 2 : " + str(pred))
 y_compare = np.argmax(y_test, axis = 1)
+print(str(y_compare))
 score = metrics.accuracy_score(y_compare, pred)
 print("Final accuracy: {}".format(score))
